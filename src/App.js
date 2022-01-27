7a5f13795d0d41b5f6f5821b954172a1f57c32cf
Alieltahan <alyaltahan@gmail.com>
2022-01-27 22:27:57 +0200

    Done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
+import { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
+class App extends Component {
+  state = { 
+    step: 1
+   } 
+
+   stepMap = [1,2,3]
+   handleNext=()=> {
+     this.setState({step: this.state.step + 1})
+   }
+   
+   handlePrev=()=> {
+     this.setState({step: this.state.step - 1})
+   }
+   
+   handleStyles=(i)=>{
+     const {step} = this.state;
+     return (step > i) ? 'step active complete  container' : (step<i)? 'step container' : 'step active  container';
+   }
+   handleProress = () =>{
+    const {step} = this.state
+    
+    return ((step -1) / (this.stepMap.length-1)) * 100 ;
+   }
+
+    render() {
+      const {step} = this.state;
+
+
+    return (<>
+      <div id="progress">
+    <div 
+    style={{width: `${this.handleProress()}%`}} 
+    id="progress-bar"></div>
+    <ul id="progress-num">
+      {this.stepMap.map((s,i) => <div className='container'> <li key={s} className={this.handleStyles(i+1)}>
+        {step <= s ? s : '✓'}
+      </li></div> )}
+
+    </ul>
+  </div>
+  <button  onClick={this.handlePrev} className="btn" disabled={step === 1}>Prev</button>
+  <button onClick={this.handleNext} className="btn">Next</button>
+    </>);
+  }
+}
+ 
 export default App;
